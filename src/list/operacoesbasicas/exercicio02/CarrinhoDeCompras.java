3067e2ac94f39337c98d0913b078abbc217ac328
KauaNilton <kaua4nilton@gmail.com>
2023-09-02 14:56:30 -0300

    Resolvendo exercicios de ordenação e pesquisa

--- src/list/operacoesbasicas/exercicio02/CarrinhoDeCompras.java
+++ src/list/operacoesbasicas/exercicio02/CarrinhoDeCompras.java
@@ -56,4 +56,17 @@ public class CarrinhoDeCompras {
                 "itemList = " + itemList +
                 '}';
     }
+    public static void main(String[] args) {
+        CarrinhoDeCompras pd = new CarrinhoDeCompras();
+
+        pd.adicionaritem("nescau", 33.9, 2);
+        pd.adicionaritem("feijao", 36.7, 4);
+        pd.adicionaritem("Caneta", 2.99, 2);
+        pd.adicionaritem("caderno", 26.67, 1);
+        pd.exibirItens();
+
+        //  pd.removerItem("nescau");
+        // pd.exibirItens();
+        System.out.printf("Total: %.2f", pd.calcularValorTotal());
+    }
 }
---
+++ src/list/ordenacao/exercicio02/OrdenacaoNumeros.java
@@ -0,0 +1,60 @@
+package list.ordenacao.exercicio02;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoNumeros {
+    private List<Integer> numeroList;
+
+    public OrdenacaoNumeros() {
+        this.numeroList = new ArrayList<>();
+    }
+
+    public void adicionarNumero(int numero) {
+       this.numeroList.add(numero);
+    }
+
+    public List<Integer> ordenarAscendente() {
+        List<Integer> numerosAscendentes = new ArrayList<>(this.numeroList);
+        if(!numeroList.isEmpty()) {
+            Collections.sort(numerosAscendentes);
+            return numerosAscendentes;
+        } else {
+            throw new RuntimeException("A lista es´ta vazia!");
+        }
+    }
+
+    public List<Integer> ordenarDescendente() {
+        List<Integer> numerosAscendentes = new ArrayList<>(numeroList);
+        if(!numeroList.isEmpty()) {
+            numerosAscendentes.sort(Collections.reverseOrder());
+            return numerosAscendentes;
+        } else {
+            throw new RuntimeException("A lista está vazia!");
+        }
+    }
+    public void exibirList() {
+        if(!numeroList.isEmpty()) {
+            System.out.println(this.numeroList);
+        } else {
+            System.out.println("A lista está vazia!");
+        }
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
+
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(0);
+        ordenacaoNumeros.adicionarNumero(10);
+        ordenacaoNumeros.adicionarNumero(17);
+        ordenacaoNumeros.adicionarNumero(11);
+
+        ordenacaoNumeros.exibirList();
+        System.out.println(ordenacaoNumeros.ordenarAscendente());
+        System.out.println(ordenacaoNumeros.ordenarDescendente());
+    }
+}
+
